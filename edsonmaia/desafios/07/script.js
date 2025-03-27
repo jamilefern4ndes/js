b1440564ccfc7060485293f19dccc88e12f52fb8
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2025-03-27 15:37:36 -0300

    ajuste na conversão

--- edsonmaia/desafios/07/script.js
+++ edsonmaia/desafios/07/script.js
@@ -1,3 +1,20 @@
+
+//ativação do botão
+function ativarbotao() {
+    const valor = document.getElementById("val").value
+    const opcao = document.getElementById("opcao").value
+    const botao = document.getElementById("btconverter")
+
+    if (valor > 0 && opcao !== "---") {
+        botao.disabled = false
+    } else {
+        botao.disabled = true
+    }
+}
+document.getElementById("val").addEventListener("input", ativarbotao);
+document.getElementById("opcao").addEventListener("change", ativarbotao);
+
+//conversão da moeda
 function converter() {
     let reais = parseFloat(document.querySelector('#val').value)
     let moeda = document.querySelector('#opcao').value
@@ -8,13 +25,16 @@ function converter() {
     const eur = 6.0
     const lib = 7.4
 
+    
     if (moeda === 'dolar') {
-        valorConvertido.innerHTML = `${reais} Reais em Dólar: ${(reais * dol).toFixed(2)}`
+        valorConvertido.innerHTML = `${reais} Reais em Dólar: $ ${(reais / dol).toFixed(2)}`
     } else if (moeda === 'euro') {
-        valorConvertido.innerHTML = `${reais} Reais em Euro: ${(reais * eur).toFixed(2)}`
+        valorConvertido.innerHTML = `${reais} Reais em Euro: €  ${(reais / eur).toFixed(2)}`
     } else if (moeda === 'Libra') {
-        valorConvertido.innerHTML = `${reais} Reais em Libra: ${(reais * lib).toFixed(2)}`
-    } else {
-        valorConvertido.innerHTML = 'Por favor, selecione uma moeda válida.'
+        valorConvertido.innerHTML = `${reais} Reais em Libra: £ ${(reais / lib).toFixed(2)}`
     }
+    //estilização
+    valorConvertido.style.backgroundColor = 'rgba(0, 0, 0, 0.18)'
+    valorConvertido.style.padding = '10px'
+    valorConvertido.style.borderRadius = '5px'
 }
