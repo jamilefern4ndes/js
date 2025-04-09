476f2ab3aa258ff3212c225822ab39772a66c69e
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2025-04-08 21:17:26 -0300

    verão funcional

--- edsonmaia/desafios/12citacao/script.js
+++ edsonmaia/desafios/12citacao/script.js
@@ -1,9 +1,28 @@
+let btncriar = document.querySelector('#criar')
+let dataInput = document.querySelector('#data')
+
+
+dataInput.addEventListener('change', function () {
+    if (dataInput.value !== '') {
+        btncriar.disabled = false
+    } else {
+        btncriar.disabled = true
+    }
+})
+
 
 let tela = document.querySelector('#telaPrincipal')
 let newsection = document.createElement('section')
 
 //criar uma nova sessão
-function novaSessao() {
+function criarImagem() {
+    let data = document.querySelector('#data').value
+    //formatar data
+    let partesData = data.split("-");
+    let dataFormatada = `${partesData[2]}-${partesData[1]}-${partesData[0]}`
+
+    let mensagem = document.querySelector('#mensagem').value
+    let autor = document.querySelector('#autor').value
     tela.innerHTML = ""
     tela.appendChild(newsection)
     newsection.setAttribute('class', 'novasessao')
@@ -17,29 +36,23 @@ function novaSessao() {
         location.reload() 
     });
 
-    criarMensagem()
-}
-
-//criar uma mensagem
-function criarMensagem() {
-    let data = document.querySelector('#data').value
-    console.log(data)
-    let mensagem = document.querySelector('#mensagem').value
-    let autor = document.querySelector('#autor').value
-
-    // Divisórias
+    //divisoes
     let divData = document.createElement('div')
     divData.setAttribute('class', 'divData')
-    divData.textContent = `Data: ${data}`
+    divData.textContent = dataFormatada
     newsection.appendChild(divData)
-
+    
     let divMsg = document.createElement('div')
     divMsg.setAttribute('class', 'divMsg')
-    divMsg.textContent = `Mensagem: ${mensagem}`
+    divMsg.textContent = `"${mensagem}"`
     newsection.appendChild(divMsg)
-
+    
     let divAutor = document.createElement('div')
     divAutor.setAttribute('class', 'divAutor')
-    divAutor.textContent = `Autor: ${autor}`
+    divAutor.textContent = `- ${autor}`
     newsection.appendChild(divAutor)
+
 }
+
+
+
