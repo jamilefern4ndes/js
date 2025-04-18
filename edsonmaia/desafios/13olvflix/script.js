f4f82ef6eab70b16f2ec60a6d7c89e75d16e2f90
Jamile Fernandes <jamile.fern4ndes@gmail.com>
2025-04-18 19:59:55 -0300

    versão simples
    site versão simples e funcional (banner não funcional)

--- edsonmaia/desafios/13olvflix/script.js
+++ edsonmaia/desafios/13olvflix/script.js
@@ -1,3 +1,5 @@
+
+//ajustes de tema
 let tela = document.querySelector('.fundo')
 function temaDark(){
     tela.classList.add('temaDark')
@@ -6,4 +8,22 @@ function temaDark(){
 function temaClaro(){
     tela.classList.add('temaClaro')
     tela.classList.remove('temaDark')
+}
+
+//banners
+
+//carrossel
+let numDeItens = 3
+function show(i, iLista){
+    let n = i 
+    numdeItens = numDeItens + i
+    // console.log(n + " " + numDeItens)
+
+    let listaSelected = document.querySelector('#lista'+iLista)
+    if (i == +1){
+        listaSelected.scrollBy(100, 0)
+    }
+    if (i == -1){
+        listaSelected.scrollBy(-100, 0)
+    }
 }
